f6580935c8aa7158768efed38bdb5efcbd27cd2a
nguyendothanhtruc <trucnguyen0256@gmail.com>
2021-07-12 22:29:09 +0700

    format & test

--- A_Star.py
+++ A_Star.py
@@ -26,7 +26,7 @@ class Node:
         return False
 
 
-
+#Handle input 
 def convertToMatrix(filepath:str):
     #Open file bitmap => convert to array greyscale
     img = Image.open(filepath)
@@ -34,28 +34,6 @@ def convertToMatrix(filepath:str):
     map = np.asarray(bitmap,dtype='int')
     bitmap.close()
     return map
-
-def colorBMP(fileInput:str,fileOutput:str, S:Node, G:Node):
-    img = Image.open(fileInput)
-
-    color = (255,0,0)
-    
-    while not G.isEqual(S):
-        img.putpixel((G.x,G.y),color)
-        G = G.parent
-    img.putpixel((G.x,G.y),color)
-
-    img.save(fileOutput)
-
-def writePath(fileInput:str,fileOutput:str, S:Node, G:Node):
-    output = open('out.txt','w')
-
-    output.write(str(G.gn))
-
-    output.close()
-
-    colorBMP(fileInput,fileOutput,S,G)
-
 
 def readInput(filepath:str):
     input = open(filepath,'r',encoding = 'utf-8')
@@ -79,34 +57,70 @@ def readInput(filepath:str):
 
     return S,G,m
 
-def d(A:Node,B:Node,map):
+#Output to file
+def colorBMP(G:Node):
+    img = Image.open(imageInput)
+
+    color = (255,0,0)
+    
+    while not G.isEqual(Start):
+        img.putpixel((G.x,G.y),color)
+        G = G.parent
+        print(str(G.x),str(G.y),sep = ',', end = '->')
+    img.putpixel((G.x,G.y),color)
+
+    img.save(imageOutput)
+
+def writePath(G:Node, closed_list):
+    output = open(fileOutput,'w')
+
+    if G != None:
+        distance = str(G.gn) + '\n'
+        count = str(np.count_nonzero(closed_list)) + '\n'
+        s = [distance, count]
+        
+        output.writelines(s)
+
+        output.close()
+
+        colorBMP(G)
+    else:
+        output.write("No solution / Error")
+
+        output.close()
+
+    
+
+#Calculate distance from A->B:
+def distance(A:Node,B:Node):
     deltaA = map[A.y][A.x] - map[B.y][B.x]
     
     AB = np.sqrt(np.square(B.x-A.x) + np.square(B.y - A.y))
  
     return AB + abs(deltaA)*(0.5*np.sign(deltaA)+1)
 
-def checkHeight(A:Node, B:Node, m: int):
+#Check if can climb from A->B:
+def checkHeight(A:Node, B:Node):
     deltaA = map[A.y][A.x] - map[B.y][B.x]
     if abs(deltaA) <= m:
         return True
     return False
 
-def H1_Manhattan(A:Node, B:Node, map):
-    return abs(A.x-B.x)+abs(A.y-B.y) + abs(map[A.y][A.x] - map[B.y][B.x])#d(A,B,map)#
-
-def H2_Euclid(A:Node, B:Node):
-    return np.sqrt(np.square(A.x-B.x)+np.square(A.y-B.y))
-
+#Check if node in queue:
 def isIn(list, G:Node):
     for item in list:
         if item.x == G.x and item.y == G.y:
             return True,item
 
     return False,G
-def isHere(list,G:Node):
-    if list[G.y][G.x]>0:
-        return True, G
+
+#Heuristic functions:
+def H1_Manhattan(A:Node, B:Node):
+    return abs(A.x-B.x)+abs(A.y-B.y) + abs(map[A.y][A.x] - map[B.y][B.x])#d(A,B,map)#
+
+def H2_Euclid(A:Node, B:Node):
+    return np.sqrt(np.square(A.x-B.x)+np.square(A.y-B.y))
+
 
 def generateSuccessor(currentNode:Node):
     #Current_node coordinates
@@ -119,139 +133,160 @@ def generateSuccessor(currentNode:Node):
     #Generates successors
     if y-1 > 0: #If not top border
         A = Node(x, y-1)
-        if (checkHeight(currentNode,A,m)):
+        if (checkHeight(currentNode,A)):
             successor.append(A)
 
         if x + 1 < column: #If not right border
             A = Node(x+1,y-1)
-            if (checkHeight(currentNode,A,m)):
+            if (checkHeight(currentNode,A)):
                 successor.append(A)
 
             A = Node(x+1,y)
-            if (checkHeight(currentNode,A,m)):
+            if (checkHeight(currentNode,A)):
                 successor.append(A)
 
         if x - 1 > 0: #If not left border
             A = Node(x-1,y-1)
-            if (checkHeight(currentNode,A,m)):
+            if (checkHeight(currentNode,A)):
                 successor.append(A)
 
             A = Node(x-1,y)
-            if (checkHeight(currentNode,A,m)):
+            if (checkHeight(currentNode,A)):
                 successor.append(A)   
 
     #If not bottom border
     if y + 1 < row: 
         A = Node(x,y+1)
-        if (checkHeight(currentNode,A,m)):
+        if (checkHeight(currentNode,A)):
             successor.append(A)
 
         if x + 1 < column:  #If not right border
             A = Node(x+1,y+1)
-            if (checkHeight(currentNode,A,m)):
+            if (checkHeight(currentNode,A)):
                 successor.append(A)
 
         if x - 1 > 0:   #If not left border
             A = Node(x-1,y+1)
-            if (checkHeight(currentNode,A,m)):
+            if (checkHeight(currentNode,A)):
                 successor.append(A)
         
     #If not right border
     if x + 1 < column: 
         A = Node(x+1,y)
-        if (checkHeight(currentNode,A,m)):
+        if (checkHeight(currentNode,A)):
                 successor.append(A)
 
     #If not left border
         if x - 1 > 0: 
             A = Node(x-1,y)
-            if (checkHeight(currentNode,A,m)):
+            if (checkHeight(currentNode,A)):
                 successor.append(A)  
 
     return successor   
 
-def A_Star(map:np.array,S:Node,G:Node, m:int, heuristic,fileInput, fileOutput):
-    queue:list = [] #open list
+def A_Star(heuristic):
+    #Open list to store node have not been explored
+    queue:list = [] 
+
+    return writePath(None,queue)
+    #initialize matrix store g(n) value of each node
     row,col=map.shape
     closed_list=np.zeros((row,col))
 
-    #Put S to open_list
-    queue.append(S)
-    S.fn = heuristic(S,G,map)
+    #Put Start node to open_list
+    queue.append(Start)
+    Start.fn = heuristic(Start,Goal)
 
 
     currentNode:Node
 
     #While open_list is not empty
+
     dup=closed_list
     while (queue):
         currentNode = queue[0]
 
         #Reach goal -> draw path
-        if currentNode.isEqual(G): 
-            return writePath(fileInput, fileOutput, S,currentNode)
+        if currentNode.isEqual(Goal): 
+            return writePath(currentNode,closed_list)
 
+        #Generate all successors of this node
         successor = generateSuccessor(currentNode)
 
         #For each node in successros[]
         for node in successor:
-            #Calculate successors.f(n)
-            successor_cost = currentNode.gn + d(currentNode,node,map)
+            #Calculate cost from current node to this successor
+            successor_cost = currentNode.gn + distance(currentNode,node)
 
+            #If node is in queue and has g(n) <= successor_cost => Skip this node
+            #Otherwise, update node's value
             check,node = isIn(queue,node)
             if check:
-                if node.gn <= successor_cost: continue
+                if node.gn <= successor_cost: 
+                    continue
 
             else: 
-                #check,node = isIn(closed_list,node)
-                check=isHere(closed_list,node)
-                if check:
-                    if node.gn <= successor_cost: 
+               
+                #If node is in closed_list has g(n) <= successor_cost => Skip this node
+                #Otherwise, add this node back to queue
+                if closed_list[node.y][node.x] > 0:
+                    if closed_list[node.y][node.x] <= successor_cost: 
                         continue
 
                     queue.append(node)
 
-                   # closed_list.remove(node)
                     closed_list[node.y][node.x]=0
  
 
+                #Update h(n) and add this node to queue
                 else:
-                	node.hn = heuristic(node,G,map) #heuristic 
+                	node.hn = heuristic(node,Goal) 
 
                 	queue.append(node)
 
+            #Update node's value
             node.gn = successor_cost
             node.fn = node.gn + node.hn
             node.parent = currentNode
                
         #Add current_node to closed list
-        #closed_list.append(currentNode)
         closed_list[currentNode.y][currentNode.x]=currentNode.gn
 
-        #Remove current node from open_list
+        #Remove current node from queue
         queue.remove(currentNode)
-
 
         #Sort queue according to f(n) value
         queue.sort(key = lambda Node: Node.fn)
+
         #test
         dup=closed_list
 
 
     #If the last node is not goal -->No solution or error
-    if currentNode != G: print ("Can not find solution")
+    if currentNode != Goal: writePath(None,closed_list)
     print(dup)
 
-#MAIN:
 
-map = convertToMatrix('map.bmp')
-print(map)
+#---------MAIN--------:
 
+#Global variable
+imageInput = 'map.bmp'
+fileInput = 'input.txt'
 
-S,G,m = readInput('input.txt')
+map = convertToMatrix(imageInput)
 
+Start,Goal,m = readInput('input.txt')
 
-A_Star(map,S,G,m,H1_Manhattan,'map.bmp', 'out.bmp')
+#function:
 
+imageOutput = 'map1.bmp'
+fileOutput = 'output1.txt'
+A_Star(H1_Manhattan)
 
-#colorBMP('map.bmp','new.bmp')
+imageOutput = 'map2.bmp'
+fileOutput = 'output2.txt'
+#A_Star(H2_Euclid)
+
+imageOutput = 'map2.bmp'
+fileOutput = 'output2.txt'
+
